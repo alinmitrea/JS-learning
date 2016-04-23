106f09c2ffc7867b640c5a93738ac39c43f4a5ac
alinmitrea <alinmitrea@gmail.com>
2016-04-23 15:21:20 +0200

    Hoisting, IIFEs and Closure

--- js/learning.js
+++ js/learning.js
@@ -19,3 +19,27 @@ function testSubtypes(){
     console.log(typeof foo());
     console.log(typeof foo.bar);
 }
+
+function testHoisting(){
+    console.log(foo()); // function can be called here even though it appears after
+
+    function foo(){ // --> the declaration is available in the entire scope! even upper before declaring it ^^^
+        return 42;
+    }
+}
+
+(function testIIFE(){
+    console.log('Immeadiately Invoked Function Expressions are executed IMMEDIATELY!!!')
+})();
+
+function testClosure(){
+    function makeAdder(x){
+        function add(y){
+            return y + x;
+        }
+        return add;
+    }
+
+    var plusOne = makeAdder(1);
+    console.log(plusOne(2));
+}
