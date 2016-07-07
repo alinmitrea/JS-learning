1913fb663e3b927151de802825118400b39f0245
alinmitrea <alinmitrea@gmail.com>
2016-07-07 10:33:45 +0200

    AngularJs - Gulp - exercise

---
+++ coursera/conFusion-angularJS/npm_alin_install_tools.bat
@@ -0,0 +1,29 @@
+REM ------------------------------------------------------------------------------
+REM GRUNT
+REM ------------------------------------------------------------------------------
+npm install -g grunt-cli
+npm install grunt --save-dev
+npm install grunt-contrib-jshint --save-dev
+npm install jshint-stylish --save-dev
+npm install time-grunt --save-dev
+npm install jit-grunt --save-dev
+grunt
+npm install grunt-contrib-copy --save-dev
+npm install grunt-contrib-clean --save-dev
+npm install grunt-contrib-concat --save-dev
+npm install grunt-contrib-cssmin --save-dev
+npm install grunt-contrib-uglify --save-dev
+npm install grunt-filerev --save-dev
+npm install grunt-usemin --save-dev
+npm install grunt-contrib-watch --save-dev
+npm install grunt-contrib-connect --save-dev
+grunt serve
+
+REM ------------------------------------------------------------------------------
+REM GULP
+REM ------------------------------------------------------------------------------
+npm install -g gulp
+npm install gulp --save-dev
+npm install jshint gulp-jshint jshint-stylish gulp-imagemin gulp-concat gulp-uglify gulp-minify-css gulp-usemin gulp-cache gulp-changed gulp-rev gulp-rename gulp-notify  browser-sync del --save-dev
+gulp
+gulp watch
