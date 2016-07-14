2c9301b466eae3e6bad7df006cff730a89c72a72
alinmitrea <alinmitrea@gmail.com>
2016-07-14 17:10:24 +0200

    Angular - assignment 2

--- coursera/conFusion-angularJS/app/scripts/app.js
+++ coursera/conFusion-angularJS/app/scripts/app.js
@@ -156,25 +156,22 @@ angular.module('confusionApp', [])
      .controller('DishCommentController', ['$scope', function($scope) {
 
                  //Step 1: Create a JavaScript object to hold the comment from the form
-                 $scope.commentGroup = {author:"", comment:"", rating:5};
+                 $scope.comment = {author:"", comment:"", rating:5};
 
                  $scope.submitComment = function () {
                      //Step 2: This is how you record the date
                      //"The date property of your JavaScript object holding the comment" = new Date().toISOString();
-                     $scope.commentGroup.date = new Date().toISOString();
+                     $scope.comment.date = new Date().toISOString();
 
                      // Step 3: Push your comment into the dish's comment array
-                     $scope.dish.comments.push($scope.commentGroup);
-                     console.log($scope.commentGroup);
+                     $scope.dish.comments.push($scope.comment);
+                     console.log($scope.comment);
 
                      //Step 4: reset your form to pristine
                      $scope.commentForm.$setPristine();
 
                      //Step 5: reset your JavaScript object that holds your comment
-                     $scope.commentGroup.rating = 5;
-                     $scope.commentGroup.comment="";
-                     $scope.commentGroup.author="";
-                     $scope.commentGroup.date="";
+                     $scope.comment = {author:"", comment:"", rating:5, date:""};
                  }
              }])
 ;
