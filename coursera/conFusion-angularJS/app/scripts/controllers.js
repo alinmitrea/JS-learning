9052b1bd8e911bb58c8ecbe510ccba440a061d60
alinmitrea <alinmitrea@gmail.com>
2016-07-29 18:00:19 +0200

    Angular - Assignment 4 - task3

--- coursera/conFusion-angularJS/app/scripts/controllers.js
+++ coursera/conFusion-angularJS/app/scripts/controllers.js
@@ -57,22 +57,18 @@ angular.module('confusionApp')
                         
         }])
 
-        .controller('FeedbackController', ['$scope', function($scope) {
-            
+        .controller('FeedbackController', ['$scope', 'feedbackFactory', function($scope, feedbackFactory) {
             $scope.sendFeedback = function() {
-                
-                console.log($scope.feedback);
-                
                 if ($scope.feedback.agree && ($scope.feedback.mychannel === "")) {
                     $scope.invalidChannelSelection = true;
                     console.log('incorrect');
                 }
                 else {
+                    feedbackFactory.getFeedback().save({id:$scope.feedback.id},$scope.feedback);
                     $scope.invalidChannelSelection = false;
                     $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
                     $scope.feedback.mychannel="";
                     $scope.feedbackForm.$setPristine();
-                    console.log($scope.feedback);
                 }
             };
         }])
