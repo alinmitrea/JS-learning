bc85dda8b430ee1836e76c8e3cd42110e9e81594
alinmitrea <alinmitrea@gmail.com>
2016-10-15 20:34:03 +0200

    User Authentication with Passport

---
+++ coursera/rest-server-passport/npm_alin_install_tools.bat
@@ -0,0 +1,9 @@
+npm install
+npm install mongoose --save
+npm install mongoose-currency --save
+npm install mongoose mongoose-currency --save
+npm install passport passport-local passport-local-mongoose --save
+npm install jsonwebtoken --save
+
+REM start the server
+npm start
