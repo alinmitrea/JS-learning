3f6f8b9f5567a350a01c16ddd8357deede9e8d1d
alinmitrea <alinmitrea@gmail.com>
2016-10-19 22:25:55 +0200

    OAuth and User Authentication

--- coursera/rest-server-passport/config.js
+++ coursera/rest-server-passport/config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+      clientID: '1145262025520186',
+      clientSecret: '2298d2b62f6e97c85782de4066e1e96d',
+      callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
 }
